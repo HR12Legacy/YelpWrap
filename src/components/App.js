8c58845b08fa7e763ac68cae8aeba8b8a95eaef9
Gustaf Brostedt <gustafbrostedt@Gustafs-MacBook-Pro.local>
2018-01-02 17:31:32 -0500

    update comments

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ export default class App extends React.Component {
     return (
       <div>
         <h1> Hello World </h1>
-        <MapContainer/>
+        <p> hello </p>
       </div>
     )
   }
