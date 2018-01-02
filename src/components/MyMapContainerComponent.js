d4b2d531ce188a7f5abcb9005726f7222e8301d0
derricktheodoregit config --global user.email derrickxtheodore@gmail.comqlsqecslsesc <derrickxtheodore@gmail.com>
2018-01-02 15:55:10 -0500

    boiler-plate googlemaps

---
+++ src/components/MyMapContainerComponent.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import MyMapComponent from './MyMapComponent.jsx';
+/**
+ * dependencies:
+ *  "googlemaps": "^1.12.0",
+    "markerclustererplus": "^2.1.4",
+    "react": "^16.2.0",
+    "react-bootstrap": "^0.31.5",
+    "react-dom": "^16.2.0",
+    "react-google-maps": "^9.4.3",
+    "react-icons": "^2.2.7",
+ */
+
+class MyMapContainerComponent extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      isMarkerShown: false
+    }
+  }
+
+  delayedShowMarker(){
+    setTimeout(() => {
+      this.setState({ isMarkerShown: true })
+    }, 1000);
+  }
+
+  render() { 
+    return (
+      <MyMapComponent
+        currentMovie={this.props.currentMovie}
+        delayedShowMarker={this.delayedShowMarker.bind(this)}
+        defaultCenter={this.props.defaultCenter}
+        markersWithInfo={this.props.markersWithInfo}
+        isMarkerShown={this.state.isMarkerShown}
+      />
+    )
+  }
+}
+
+export default MyMapContainerComponent;
