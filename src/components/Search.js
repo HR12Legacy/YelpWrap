e912293aaabfdd682bbb56d3ec7625d45b773e0b
jackie <jackie.j.www@gmail.com>
2018-01-02 16:30:58 -0500

    First Rebase

---
+++ src/components/Search.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+
+class Search extends React.Component {
+  constructor (props){
+    super(props)
+    this.state = {
+      term: ''
+    }
+  }
+
+  render(){
+    return (<div>
+      <form>
+      </form>
+    </div>);
+  }
+
+
+}
+
+
+export default Search;
+
