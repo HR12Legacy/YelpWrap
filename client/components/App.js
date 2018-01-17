c63cc0bc794215474ebb3419ee01fc21129393cf
CaioAR <cmourani12@yahoo.com>
2018-01-17 17:22:46 -0500

    changed user back to userid

--- client/components/App.js
+++ client/components/App.js
@@ -152,7 +152,7 @@ export default class App extends React.Component {
         <div className={styles.entryList}>
           <EntryList userId={ this.props.userId } list={this.state.results}/>
         </div>
-        <Chat location={this.state.location} user={this.props.user} />
+        <Chat location={this.state.location} userId={this.props.userId} />
         <Profile 
           user={this.props.user}
           list={this.state.favorites}
